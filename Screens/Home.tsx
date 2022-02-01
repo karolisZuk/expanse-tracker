7635a98c4aa350f43b49fd603e4003c124343574
karolisZuk <kar.zukas@gmail.com>
2022-02-01 17:39:22 +0100

    feat: initial set up done

---
+++ Screens/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { COLORS, FONTS, SIZES, icons } from '../constants';
+
+const Home = () => (
+  <View>
+    <Text>Home</Text>
+  </View>
+);
+
+export default Home;
